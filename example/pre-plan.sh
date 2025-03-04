cb40768cc0fd03ce0269a8d389b4695a2db9b486
Kateryna Gubchenko <79134828+kategubchenko@users.noreply.github.com>
2025-03-04 12:42:22 +0000

    Update pre-plan.sh

--- example/pre-plan.sh
+++ example/pre-plan.sh
@@ -6,4 +6,4 @@ export YOR_SIMPLE_TAGS="$(python3 $SCALR_HOOK_DIR/scripts.py get-tags -h $SCALR_
 echo $YOR_SIMPLE_TAGS
 wget https://github.com/bridgecrewio/yor/releases/download/0.1.150/yor_0.1.150_linux_amd64.tar.gz -q
 tar -xvf yor_0.1.150_linux_amd64.tar.gz
-# ./yor tag --tag-groups simple -d ./ --skip-tags git*
+./yor tag --tag-groups simple -d $SCALR_HOOK_DIR --skip-tags git*
